9db36187b2c8db1aec441f2ed0e80de9e74726ff
joao <joao.campos@unemat.br>
2022-04-21 17:43:10 -0400

    adiciona componente drawer

---
+++ src/componets/Drawer/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { styled } from '@material-ui/styles'
+import PropTypes from 'prop-types'
+
+const StyledContainer = styled('div')(() => ({
+  backgroundColor: 'black',
+  width: '100vw',
+  height: '100vh'
+}))
+export function Drawer({ children }) {
+  return <StyledContainer>{children}</StyledContainer>
+}
+
+Drawer.propTypes = {
+  children: PropTypes.node
+}
