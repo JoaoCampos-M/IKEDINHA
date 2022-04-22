460c5beb7918b091b8cb99a7e586403734dc82df
bruno.oliveira@unemat.br <antonio.barboza@viacometa.com.br>
2022-04-22 15:15:44 -0400

    Nada relevante

---
+++ src/pages/mentoria/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+export default function Mentoria() {
+  return (
+    <iframe width="320" height="240" autoPlay={true}>
+      <source
+        src="https://www.youtube.com/watch?v=wYPBkYhDVRU&t=46s"
+        type="video/mp4"
+      />
+      <source
+        src="https://www.youtube.com/watch?v=wYPBkYhDVRU&t=46s"
+        type="video/ogg"
+      />
+      Your browser does not support the video tag.
+    </iframe>
+  )
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Login from '../pages/login'
+import Mentoria from '../pages/mentoria'
 function Rotas() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/mentoria" element={<Mentoria></Mentoria>}></Route>
       </Routes>
     </BrowserRouter>
   )
